14700762d913ee6affc51050c0566477ac803640
Mihai <caprarescu_mihai@yahoo.com>
2020-01-24 13:22:10 +0200

    Firs Commit

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); //prints a text to the console
+            Console.Read(); //holds the console open until a key is pressed
+        }
+    }
+}
